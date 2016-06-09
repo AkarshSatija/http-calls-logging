d68f1290591643c11035fed55c35667d26150408
Akarsh <akarsh.satija@gmail.com>
2016-06-09 12:09:36 +0530

    Port 80

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.get('/', function(req, res) {
 });
 app.all('/log', outp);
 
-app.listen(3000, function() {
+app.listen(80, function() {
 	console.log('Example app listening on port 3000!');
 });
 
