b12026f87728dd23a92593dcc0fa51ff6bd7dfe6
Akarsh <akarsh.satija@gmail.com>
2016-06-09 11:21:01 +0530

    Procfile added for heroku

--- index.js
+++ index.js
@@ -2,37 +2,25 @@ var bodyParser = require('body-parser');
 var express = require('express');
 var app = express();
 
-
 app.use(bodyParser.urlencoded({
-		extended: true
-	}));
+	extended: true
+}));
 app.use(bodyParser.json());
-
-
-
-
-
 
 app.get('/', function(req, res) {
 	res.send('Logging is LIVE!');
 });
 app.all('/log', outp);
-/*app.post('/log', outp);
-app.put('/log', outp);
-app.delete('/log', outp);*/
 
 app.listen(3000, function() {
 	console.log('Example app listening on port 3000!');
 });
 
-
-
-
 function outp(req, res) {
 	res.json({
-	 query: req.query,
-	 headers:req.headers,
-	 body:req.body,
-	 METHOD: req.method
+		query: req.query,
+		headers: req.headers,
+		body: req.body,
+		METHOD: req.method
 	});
 }
